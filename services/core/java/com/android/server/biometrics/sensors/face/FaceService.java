07099ca7048918a5dd5a56d56075bd2248ce61d4
Oscar <osm1019@gmail.com>
2024-06-04 20:25:52 -0700

    refactor pixel props and face unlock fix

--- services/core/java/com/android/server/biometrics/sensors/face/FaceService.java
+++ services/core/java/com/android/server/biometrics/sensors/face/FaceService.java
@@ -720,10 +720,9 @@ public class FaceService extends SystemService {
                         filteredInstances = filterAvailableHalInstances(hidlSensors, aidlSensors);
 
                 final List<ServiceProvider> providers = new ArrayList<>();
-                /*
+
                 providers.addAll(getHidlProviders(filteredInstances.first));
-                providers.addAll(getAidlProviders(filteredInstances.second));
-                */
+                providers.addAll(getAidlProviders(filteredInstances.second));               
                 providers.addAll(getSenseProviders());
                 return providers;
             });
