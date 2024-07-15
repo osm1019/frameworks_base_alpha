SigmaDroid-Project/frameworks_base

SystemUI: add percentage view to brightness bar [1/2]
* Based on commit:
https://github.com/Spark-Rom/frameworks_base/commit/72351c40679a6402de3234229ef00ff2be0b8a85

Co-Authored-By: elpaablo <donopwhile1@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -98,6 +98,7 @@ public class SystemSettings {
         Settings.System.QS_HEADER_IMAGE_LANDSCAPE_ENABLED,
         Settings.System.QS_HEADER_IMAGE_PADDING_SIDE,
         Settings.System.QS_HEADER_IMAGE_PADDING_TOP,
-        Settings.System.QS_HEADER_IMAGE_URI
+        Settings.System.QS_HEADER_IMAGE_URI,
+        Settings.System.SHOW_BRIGHTNESS_BAR_PERCENTAGE
     };
 }

osm1019/frameworks_base_alpha

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -115,7 +115,8 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
-                Settings.System.MAX_VISIBLE_NOTIFICATION_ICONS
+                Settings.System.MAX_VISIBLE_NOTIFICATION_ICONS,
+                Settings.System.SHOW_BRIGHTNESS_BAR_PERCENTAGE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);