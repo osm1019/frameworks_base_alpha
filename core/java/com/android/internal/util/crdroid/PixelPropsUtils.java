fa58bebfa34f046bd7749efd72c7ab918a654cf7
Oscar <osm1019@gmail.com>
2024-08-01 10:15:00 -0700

    Update fp for device integrity

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -424,12 +424,12 @@ public class PixelPropsUtils {
         // Alter build parameters to avoid hardware attestation enforcement
         setPropValue("BRAND", "google");
         setPropValue("MANUFACTURER", "Google");
-        setPropValue("DEVICE", "sailfish");
-        setPropValue("ID", "OPM1.171019.011");
-        setPropValue("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-        setPropValue("MODEL", "Pixel");
-        setPropValue("PRODUCT", "sailfish");
-        setVersionFieldString("SECURITY_PATCH", "2017-12-05");
+        setPropValue("DEVICE", "cheetah");
+        setPropValue("ID", "AP31.240617.009");
+        setPropValue("FINGERPRINT", "google/cheetah_beta/cheetah:15/AP31.240617.009/12094726:user/release-keys");
+        setPropValue("MODEL", "Pixel 7 Pro");
+        setPropValue("PRODUCT", "cheetah_beta");
+        setVersionFieldString("SECURITY_PATCH", "2024-07-05");
     }
 
     private static boolean isCallerSafetyNet() {
